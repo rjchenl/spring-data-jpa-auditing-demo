2a362782614c2f060e0c0b942e2766d52edd286f
JayChen <JayChen@cathayholdings.com.tw>
2025-04-14 00:02:13 +0800

    Initial commit

---
+++ src/test/java/com/example/springdatajpaauditingdemo/SpringDataJpaAuditingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpaauditingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaAuditingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
